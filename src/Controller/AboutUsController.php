2d3c028e4c8eb0f3fdfc726c351338ab5fc24b3a
pgugar <156726169+pgugar@users.noreply.github.com>
2024-07-29 22:04:00 +0200

    Update AboutUsController.php

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -1,1 +1,19 @@
+<?php
+//src/Controller/AboutUsController.php
+namespace App\Controller;
+class AboutUsController extends AbstractController
+{
+    /**
+     * @Route("/aboutUs", name="aboutUs")
+     */
+    public function contact(): Response
+    {
+        // Obtiene el usuario autenticado actualmente
+        $usuario = $this->getUser();
 
+        return $this->render('aboutUs.html.twig', [
+            'usuario' => $usuario,
+        ]);
+    }
+}
+?>
